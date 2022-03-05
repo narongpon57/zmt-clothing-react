eac31b11eec3477c006b4615ebcd670a81d6c1e6
Narongpon Suthimakphol <narongpon.dev@gmail.com>
2022-03-06 01:46:16 +0700

    intregated firebase into application

---
+++ src/firebase/firebase.util.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+}
+
+console.log(config)
+
+firebase.initializeApp(config)
+
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+
+const provier = new firebase.auth.GoogleAuthProvider()
+provier.setCustomParameters({ prompt: 'select_account ' })
+export const signInWithGoogle = () => auth.signInWithPopup(provier)
+
+export default firebase
